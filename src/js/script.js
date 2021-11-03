21e1c15940f672c1d64242247afd53182cc1969a
Adam Stępnicki <adamstepnicki@op.pl>
2021-11-03 18:41:55 +0100

    Consult processOrder()

--- src/js/script.js
+++ src/js/script.js
@@ -95,6 +95,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -190,6 +191,17 @@
             price -= option.price;  
           } 
         
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          
+          if(optionImage){
+            console.log('optionImage: ', optionImage);
+             
+            if (formData[paramId] && formData[paramId].includes[optionId]){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
         }
       } 
 
